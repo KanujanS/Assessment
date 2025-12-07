eb3f2f8d3264a19a124357189e3f03e4391d2fca
KanujanS <kanujansk@gmail.com>
2025-12-07 23:59:50 +0530

    MongoDB connected

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API Working')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
